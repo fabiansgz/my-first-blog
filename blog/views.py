332a04260334cf41ebcce4098c96b762ecca5fcd
fabiansgz <fabiansgz@gmail.com>
2020-06-17 18:33:19 -0400

    otro error

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
+from django import forms
+from django.http import HttpResponseRedirect
 from .forms import PostForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
